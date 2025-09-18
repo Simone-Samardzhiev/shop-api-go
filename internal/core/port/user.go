bc3048d73ec0b51eebf34f76009bbef848c2297c
Simone <simonesamardzhiev@gmail.com>
2025-09-18 22:41:20 +0300

    Feat: Add user ports

---
+++ internal/core/port/user.go
@@ -0,0 +1,17 @@
+package port
+
+import (
+	"context"
+	"shop-api-go/internal/core/domain"
+)
+
+// UserRepository is an interface for interacting with user-related data.
+type UserRepository interface {
+	// CreateUser inserts a new user into the database.
+	CreateUser(ctx context.Context, user *domain.User) error
+}
+
+// UserService is an interface for interacting with user-related business logic.
+type UserService interface {
+	Register(ctx context.Context, user *domain.User) (domain.User, error)
+}
