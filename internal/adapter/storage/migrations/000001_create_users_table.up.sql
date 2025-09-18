22530a5591c72fcd298fee8aaeaeb8e5e7ad4e4e
Simone <simonesamardzhiev@gmail.com>
2025-09-18 17:19:57 +0300

    Feat: Add migration for adding users table

---
+++ internal/adapter/storage/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TYPE user_role_enum AS ENUM ('admin', 'client', 'delivery', 'warehouse');
+
+CREATE TABLE users
+(
+    id         UUID PRIMARY KEY,
+    username   VARCHAR(255) CHECK ( length(username) > 8 ) NOT NUlL UNIQUE,
+    email      VARCHAR(255) CHECK ( length(email) > 8 )    NOT NULL UNIQUE,
+    password   VARCHAR(255)                                NOT NULL,
+    role       user_role_enum                              NOT NULL DEFAULT ('client'),
+    created_at TIMESTAMP                                   NOT NULL DEFAULT (now()),
+    updated_at TIMESTAMP                                   NOT NULL DEFAULT (now())
+);
