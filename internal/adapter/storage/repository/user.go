e5714b678a3b5b642f3692dcd346c958afb0b41c
Simone <simonesamardzhiev@gmail.com>
2025-09-18 23:54:44 +0300

    Feat: Add user repository implementation

---
+++ internal/adapter/storage/repository/user.go
@@ -0,0 +1,34 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"shop-api-go/internal/core/domain"
+)
+
+// UserRepository implements port.UserRepository and provides
+// access to postgres database.
+type UserRepository struct {
+	db *sql.DB
+}
+
+func (r *UserRepository) CreateUser(ctx context.Context, user *domain.User) error {
+	_, err := r.db.ExecContext(
+		ctx,
+		`INSERT INTO users (id, username, email, password)
+		VALUES ($1, $2, $3, $4)`,
+		user.Id,
+		user.Username,
+		user.Email,
+		user.Password,
+	)
+
+	return err
+}
+
+// NewUserRepository creates a new UserRepository instance
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
