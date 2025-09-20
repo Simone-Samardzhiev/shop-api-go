a585cfd8f74a832499d0ace3b5b944334cdadbab
Simone <simonesamardzhiev@gmail.com>
2025-09-20 14:05:33 +0300

    Feat: Add startup in /cmd/http

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"log"
+	"shop-api-go/internal/adapter/config"
+	"shop-api-go/internal/adapter/handler/http"
+	"shop-api-go/internal/adapter/storage"
+	"shop-api-go/internal/adapter/storage/repository"
+	"shop-api-go/internal/core/service"
 
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	container, err := config.New()
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+
+	db, err := storage.New(container.Database)
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	userRepository := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepository)
+	userHandler := http.NewUserHandler(userService)
+
+	router := http.NewRouter(userHandler)
+	err = router.Start(":8080")
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
---
+++ internal/adapter/handler/http/user.go
@@ -0,0 +1,47 @@
+package http
+
+import (
+	"net/http"
+	"shop-api-go/internal/core/domain"
+	"shop-api-go/internal/core/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+// UserHandler represents HTTP handler for user-related requests.
+type UserHandler struct {
+	us *service.UserService
+}
+
+// NewUserHandler returns a new UserHandler instance.
+func NewUserHandler(us *service.UserService) *UserHandler {
+	return &UserHandler{
+		us: us,
+	}
+}
+
+// registerRequest represent a request body for creating a user.
+type registerRequest struct {
+	Email    string `json:"email" binding:"required,email,min_bytes=8,max_bytes=255"`
+	Username string `json:"username" binding:"required,min_bytes=8,max_bytes=255"`
+	Password string `json:"password" binding:"required,password"`
+}
+
+func (uh *UserHandler) Register(c *gin.Context) {
+	var req registerRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	err := uh.us.Register(c, &domain.User{
+		Email:    req.Email,
+		Username: req.Username,
+		Password: req.Password,
+	})
+	if err != nil {
+		_ = c.Error(err)
+	}
+
+	c.Status(http.StatusCreated)
+}
