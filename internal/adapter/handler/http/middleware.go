558006bb19b41522c53569993591545221da46d1
Simone <simonesamardzhiev@gmail.com>
2025-09-24 18:34:42 +0300

    Feat: Add JWT middleware

--- internal/adapter/handler/http/middleware.go
+++ internal/adapter/handler/http/middleware.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"shop-api-go/internal/core/domain"
+	"shop-api-go/internal/core/port"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
@@ -65,5 +67,42 @@ func errorHandler() gin.HandlerFunc {
 			}
 
 		}
+	}
+}
+
+// jwtMiddleware is a middleware used to authenticate user by JWT.
+//
+// Note: The middleware support role specification and token type specification.
+func jwtMiddleware(generator port.TokenGenerator, role domain.UserRole, tokenType domain.TokenType, key string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		header := c.GetHeader("Authorization")
+		if header == "" || !strings.HasPrefix(header, "Bearer ") {
+			_ = c.Error(domain.ErrMalformedToken)
+			c.Abort()
+			return
+		}
+
+		tokenString := strings.TrimPrefix(header, "Bearer ")
+		token, err := generator.ParseToken(tokenString)
+		if err != nil {
+			_ = c.Error(err)
+			c.Abort()
+			return
+		}
+
+		if token.UserRole != role {
+			_ = c.Error(domain.ErrMalformedToken)
+			c.Abort()
+			return
+		}
+
+		if token.TokenType != tokenType {
+			_ = c.Error(domain.ErrInvalidToken)
+			c.Abort()
+			return
+		}
+
+		c.Set(key, token)
+		c.Next()
 	}
 }
