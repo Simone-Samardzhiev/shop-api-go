a336cd5d221b78e667d786469308ed7c1e6311a6
Simone <simonesamardzhiev@gmail.com>
2025-09-25 21:35:21 +0300

    Feat: Make router check for environment to set correct gin mode

--- internal/adapter/handler/http/router.go
+++ internal/adapter/handler/http/router.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"shop-api-go/internal/adapter/config"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -10,11 +12,18 @@ type Router struct {
 	*gin.Engine
 }
 
-func NewRouter(userHandler *UserHandler) *Router {
+func NewRouter(appConfig *config.AppConfig, userHandler *UserHandler) *Router {
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		_ = v.RegisterValidation("password", validatePassword)
 		_ = v.RegisterValidation("min_bytes", validateMinBytesLength)
 		_ = v.RegisterValidation("max_bytes", validateMaxBytesLength)
+	}
+
+	switch appConfig.Environment {
+	case config.Production:
+		gin.SetMode(gin.ReleaseMode)
+	case config.Development:
+		gin.SetMode(gin.DebugMode)
 	}
 
 	r := gin.New()
