96780df13ea204354bbf78c6b8cbc3e80ab9ff95
Simone <simonesamardzhiev@gmail.com>
2025-09-28 01:00:12 +0300

    Refactor: Change password.go to return domain errors

--- internal/core/util/password.go
+++ internal/core/util/password.go
@@ -1,13 +1,21 @@
 package util
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"shop-api-go/internal/core/domain"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 // HashPassword hashes input password using bcrypt.
 func HashPassword(password string) ([]byte, error) {
-	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, domain.ErrInternalServerError
+	}
+	return hash, nil
 }
 
 // ComparePassword compares input password and hash.
-func ComparePassword(password, hash string) error {
-	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+func ComparePassword(password, hash string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil
 }
