1f198f70dc7309fb42faa15561f83a4ce94f84ef
Simone <simonesamardzhiev@gmail.com>
2025-10-13 13:55:23 +0300

    refactor(repository): Update logging in `postgres.TokenRepository`

--- internal/adapter/storage/postgres/repository/auth.go
+++ internal/adapter/storage/postgres/repository/auth.go
@@ -31,14 +31,16 @@ func (t *TokenRepository) AddToken(ctx context.Context, token *domain.Token) err
 		token.ExpiresAt,
 	)
 	if err != nil {
-		zap.L().Error("postgres/TokenRepository.AddToken failed",
-			zap.String("tokenId", token.Id.String()),
-			zap.String("userId", token.UserId.String()),
-			zap.String("tokenType", string(token.TokenType)),
-			zap.String("userRole", string(token.UserRole)),
-			zap.String("expiresAt", token.ExpiresAt.String()),
-			zap.Error(err),
-		)
+		zap.L().
+			Error(
+				"postgres/TokenRepository.AddToken failed",
+				zap.String("tokenId", token.Id.String()),
+				zap.String("userId", token.UserId.String()),
+				zap.String("tokenType", string(token.TokenType)),
+				zap.String("userRole", string(token.UserRole)),
+				zap.String("expiresAt", token.ExpiresAt.String()),
+				zap.Error(err),
+			)
 		return domain.ErrInternalServerError
 	}
 	return nil
@@ -47,16 +49,22 @@ func (t *TokenRepository) AddToken(ctx context.Context, token *domain.Token) err
 func (t *TokenRepository) DeleteToken(ctx context.Context, id uuid.UUID) error {
 	result, err := t.db.ExecContext(ctx, "DELETE FROM tokens WHERE id = $1", id)
 	if err != nil {
-		zap.L().Error("postgres/TokenRepository.DeleteToken failed",
-			zap.String("id", id.String()),
-			zap.Error(err),
-		)
+		zap.L().
+			Error(
+				"postgres/TokenRepository.DeleteToken failed",
+				zap.String("id", id.String()),
+				zap.Error(err),
+			)
 		return domain.ErrInternalServerError
 	}
 
 	rowsAffected, err := result.RowsAffected()
 	if err != nil {
-		zap.L().Error("postgres/TokenRepository.DeleteToken failed", zap.Error(err))
+		zap.L().
+			Error(
+				"postgres/TokenRepository.DeleteToken failed getting affected rows",
+				zap.Error(err),
+			)
 		return domain.ErrInternalServerError
 	}
 
@@ -69,7 +77,12 @@ func (t *TokenRepository) DeleteToken(ctx context.Context, id uuid.UUID) error {
 func (t *TokenRepository) DeleteAllTokensByUserId(ctx context.Context, userId uuid.UUID) error {
 	_, err := t.db.ExecContext(ctx, "DELETE FROM tokens WHERE user_id = $1", userId)
 	if err != nil {
-		zap.L().Error("postgres/TokenRepository.DeleteAllTokensByUserId failed")
+		zap.L().
+			Error(
+				"postgres/TokenRepository.DeleteAllTokensByUserId failed",
+				zap.String("userId", userId.String()),
+				zap.Error(err),
+			)
 		return domain.ErrInternalServerError
 	}
 	return nil
@@ -78,7 +91,11 @@ func (t *TokenRepository) DeleteAllTokensByUserId(ctx context.Context, userId uu
 func (t *TokenRepository) DeleteExpiredTokens() error {
 	_, err := t.db.Exec("DELETE FROM tokens WHERE expires < NOW()")
 	if err != nil {
-		zap.L().Error("postgres/TokenRepository.DeleteExpiredTokens failed", zap.Error(err))
+		zap.L().
+			Error(
+				"postgres/TokenRepository.DeleteExpiredTokens failed",
+				zap.Error(err),
+			)
 		return domain.ErrInternalServerError
 	}
 	return nil
