12e0bf08bbdc057dccbbaef42e171bebb13a5e9b
Simone <simonesamardzhiev@gmail.com>
2025-10-13 12:08:27 +0300

    feat(domain): Update product entities

--- internal/core/domain/product.go
+++ internal/core/domain/product.go
@@ -7,17 +7,38 @@ import (
 	"github.com/shopspring/decimal"
 )
 
+// CategorySection is an entity representing a category section.
+type CategorySection struct {
+	Id   uuid.UUID
+	Name string
+}
+
+// Category is an entity representing a category.
+type Category struct {
+	Id        uuid.UUID
+	Name      string
+	SectionId uuid.UUID
+}
+
+// Subcategory is an entity representing a category
+type Subcategory struct {
+	Id         uuid.UUID
+	Name       string
+	CategoryID uuid.UUID
+}
+
 // Product is an entity representing a product.
 type Product struct {
-	Id          uuid.UUID
-	Name        string
-	Description string
-	Price       decimal.Decimal
-	Rating      decimal.Decimal
-	Count       int
-	ImageUrl    string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	Id            uuid.UUID
+	Name          string
+	Description   string
+	Price         decimal.Decimal
+	Rating        decimal.Decimal
+	Count         int
+	ImageUrl      string
+	Subcategories []Subcategory
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
 }
 
 // NewProduct creates a new Product instance.
@@ -29,18 +50,20 @@ func NewProduct(
 	rating decimal.Decimal,
 	count int,
 	imageUrl string,
+	subcategories []Subcategory,
 	createdAt time.Time,
 	updatedAt time.Time,
 ) *Product {
 	return &Product{
-		Id:          id,
-		Name:        name,
-		Description: description,
-		Price:       price,
-		Rating:      rating,
-		Count:       count,
-		ImageUrl:    imageUrl,
-		CreatedAt:   createdAt,
-		UpdatedAt:   updatedAt,
+		Id:            id,
+		Name:          name,
+		Description:   description,
+		Price:         price,
+		Rating:        rating,
+		Count:         count,
+		ImageUrl:      imageUrl,
+		Subcategories: subcategories,
+		CreatedAt:     createdAt,
+		UpdatedAt:     updatedAt,
 	}
 }
