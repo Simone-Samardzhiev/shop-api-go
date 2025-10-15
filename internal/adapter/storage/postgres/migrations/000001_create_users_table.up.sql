756fd320556cb6936b6e3a6936d47a433f29a8f4
Simone <simonesamardzhiev@gmail.com>
2025-10-15 14:33:58 +0300

    refactor(migrations): Refactor check constraints on users table and fix 4th down migration typos

--- internal/adapter/storage/postgres/migrations/000001_create_users_table.up.sql
+++ internal/adapter/storage/postgres/migrations/000001_create_users_table.up.sql
@@ -3,8 +3,8 @@ CREATE TYPE user_role_enum AS ENUM ('admin', 'client', 'delivery', 'warehouse');
 CREATE TABLE users
 (
     id         UUID PRIMARY KEY,
-    username   VARCHAR(255) CHECK ( length(username) > 8 ) NOT NUlL UNIQUE,
-    email      VARCHAR(255) CHECK ( length(email) > 8 )    NOT NULL UNIQUE,
+    username   VARCHAR(255) CHECK ( length(username) >= 8 ) NOT NUlL UNIQUE,
+    email      VARCHAR(255) CHECK ( length(email) >= 12 )    NOT NULL UNIQUE,
     password   VARCHAR(255)                                NOT NULL,
     role       user_role_enum                              NOT NULL DEFAULT ('client'),
     created_at TIMESTAMP                                   NOT NULL DEFAULT (now()),
--- internal/adapter/storage/postgres/migrations/000004_create_products_table.down.sql
+++ internal/adapter/storage/postgres/migrations/000004_create_products_table.down.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS product_subcategories;
+DROP TABLE IF EXISTS products_subcategories;
+DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS subcategories;
 DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS category_section;
-DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS category_sections;
